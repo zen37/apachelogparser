b87dbda17847f1a61fda097b0943080f7a90c00c
zen <zen@zen>
2021-12-23 09:40:12 -0800

    log format

---
+++ format.go
@@ -0,0 +1,50 @@
+package apachelogparser
+
+import (
+	"net"
+	"time"
+)
+
+//position of the log entry
+const (
+	IP = iota
+	Identity
+	User
+	Timestamp
+	Request
+	Status
+	Size
+	Referer
+	UserAgent
+)
+
+// Example "GET /apache_pb.gif HTTP/1.0"
+type request struct {
+	Method   string
+	Resource string //path
+	Protocol string
+}
+
+/*
+LogFormat "%h %l %u %t \"%r\" %>s %b" common
+CustomLog logs/access_log common
+*/
+type CommonLog struct {
+	IP        net.IP
+	Identity  string
+	User      string
+	Timestamp time.Time
+	Request   request
+	Status    int
+	Size      int64
+}
+
+/*
+LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"" combined
+CustomLog log/access_log combined
+*/
+type CombinedLog struct {
+	Common    CommonLog
+	Referer   string
+	UserAgent string
+}
