fc0ef2a1ddc7eb677dcc3a26a43efc074b499db5
zen <zen@zen>
2021-12-31 08:37:10 -0800

    update tests

--- parse_test.go
+++ parse_test.go
@@ -11,11 +11,12 @@ func TestParseLogRecord(t *testing.T) {
 	tests := []struct {
 		record string
 		log    interface{}
+		err    error
 	}{
-		{"127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 xyz", nil},
-		{"127.0.0.1 - - dda [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil},
-		{"[10/Oct/2000:13:55:36 -0700] 200 2326", nil},
-		{"[10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil},
+		{"127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 xyz", nil, ErrInvalidLog},
+		{"127.0.0.1 - - dda [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil, ErrInvalidLog},
+		{"[10/Oct/2000:13:55:36 -0700] 200 2326", nil, ErrInvalidLog},
+		{"[10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil, ErrInvalidLog},
 	}
 	t.Log("===========================================")
 	t.Log("Check non-standard log records (fewer or more fields)")
@@ -26,7 +27,7 @@ func TestParseLogRecord(t *testing.T) {
 		if !isNil(rec) {
 			t.Errorf("FAIL: returned log is not nil")
 		}
-		if !errors.Is(err, ErrInvalidLog) {
+		if !errors.Is(err, v.err) {
 			t.Errorf("FAIL: log is not marked as invalid")
 		}
 
@@ -35,10 +36,11 @@ func TestParseLogRecord(t *testing.T) {
 	tests = []struct {
 		record string
 		log    interface{}
+		err    error
 	}{
-		{"0.01 - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil},
-		{"1270.0.1 - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil},
-		{"abcdef - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil},
+		{"0.01 - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil, ErrInvalidIP},
+		{"1270.0.1 - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil, ErrInvalidIP},
+		{"abcdef - rob [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326", nil, ErrInvalidIP},
 	}
 	t.Log("===========================================")
 	t.Log("Check whether IP is invalid")
@@ -46,7 +48,7 @@ func TestParseLogRecord(t *testing.T) {
 	for _, v := range tests {
 		t.Log("checking ...", v.record)
 		_, err := ParseLogRecord(v.record)
-		if !errors.Is(err, ErrInvalidIP) {
+		if !errors.Is(err, v.err) {
 			t.Errorf("FAIL: IP not marked as invalid")
 		}
 	}
